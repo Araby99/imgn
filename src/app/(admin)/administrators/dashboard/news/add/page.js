44b348bcf63d69f14f66f7dc4cd4a0bc9ac90226
Ahmed <ahmedaraby01023980886@gmail.com>
2023-12-22 19:40:31 +0200

    debug

--- src/app/(admin)/administrators/dashboard/news/add/page.js
+++ src/app/(admin)/administrators/dashboard/news/add/page.js
@@ -93,6 +93,7 @@ export default () => {
                 imgs.forEach((img, index) => {
                     const formData = new FormData();
                     formData.append("file", img.src);
+                    console.log(process.env.NEXT_PUBLIC_UPLOAD_NEWS, process.env.NEXT_PUBLIC_CLOUD_NAME, process.env.NEXT_PUBLIC_CLOUD);
                     formData.append("upload_preset", process.env.NEXT_PUBLIC_UPLOAD_NEWS);
                     formData.append("cloud_name", process.env.NEXT_PUBLIC_CLOUD_NAME);
                     axios.post(process.env.NEXT_PUBLIC_CLOUD, formData).then(image => {
